6f10ed22e0d5306c928db991cced1dbbf501690b
Kaan Özaltan <ckozaltan@gmail.com>
2023-05-18 17:32:13 +0300

    Editing attempts

--- src/superresolution/test.py
+++ src/superresolution/test.py
@@ -1,6 +1,12 @@
 import numpy as np
+import torch
 
 direction = np.load('directions/smile.npy')
+# print(direction[0])
 direction = np.repeat(direction, 18, axis=0)
+# print(direction[4])
 direction = np.expand_dims(direction, axis=0)
-print(direction.shape)
+# print(direction[0,0])
+direction = torch.from_numpy(direction).to(torch.float32)
+# print(direction.shape)
+# print(direction[0,0])
